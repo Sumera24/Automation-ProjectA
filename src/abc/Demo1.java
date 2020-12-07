be1fdb49a408301c420e8ba1ca3af468548ddf0b
Sumera24 <sumeraqz@gmail.com>
2020-12-07 13:10:33 -0500

    This is 3rd Commit

---
+++ src/abc/Demo1.java
@@ -0,0 +1,12 @@
+package abc;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Demo1 A");
+		System.out.println("Demo1 B");
+
+	}
+
+}
